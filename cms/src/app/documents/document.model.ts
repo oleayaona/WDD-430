7c03f91638a044ef5c6d0c4af3ea54a092a52c43
Olea <oleayaona@gmail.com>
2021-05-24 12:16:55 +0800

    documents service

--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -1,6 +1,6 @@
 export class Document {
     
-    constructor(public id: number, public name: string, public description: string,
+    constructor(public id: string, public name: string, public description: string,
                 public url: string, public children: Document[]){
 
     }
